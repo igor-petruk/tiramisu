2f194e686ddeaaf01d55377c06506e2b78cdc637
Igor Petruk <igor.petrouk@gmail.com>
2012-08-16 03:02:12 +0300

    Configured JSP

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,7 @@
 package com.igorpetruk.heroku.comet
 
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.servlet.{ServletHolder, ServletContextHandler}
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
-import com.sun.jersey.spi.container.servlet.ServletContainer
-import com.sun.jersey.api.json.JSONConfiguration
-import java.util.logging.{Level, Logger}
 import org.eclipse.jetty.webapp.WebAppContext
 
 object Main {
@@ -18,12 +14,6 @@ object Main {
     server.setHandler(webApp);
     server.start
     server.join
-  }
-}
-
-class Main extends HttpServlet {
-  protected override def doGet(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
-    resp.getWriter.print("Hello my Rocksy!\n")
   }
 }
 
